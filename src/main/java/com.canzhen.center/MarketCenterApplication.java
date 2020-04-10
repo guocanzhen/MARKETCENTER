c29c25d2655fd3c31da654e20b96a77558398285
guocanzhen <2589945540@qq.com>
2020-04-10 17:38:20 +0800

    初次提交

---
+++ src/main/java/com.canzhen.center/MarketCenterApplication.java
@@ -0,0 +1,11 @@
+package com.canzhen.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketCenterApplication {
+    public static void main(String[] args){
+        SpringApplication.run(MarketCenterApplication.class,args);
+    }
+}
